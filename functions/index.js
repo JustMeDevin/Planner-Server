92e3f8da78de5bc76e376ed13cb71dd780b07be1
Devin Davies <justmedevin@gmail.com>
2018-06-07 20:34:37 +1200

    added logic for sending notifications on event changes

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,94 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+//const userFields = { "CYCClass" : ["speaker","chair","hosts", "supper"], "CYCActivity": ["hosts", "organisers"]}
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.notifyUsers = functions.database.ref('/plans/{ecclesiaId}/{year}/{date}/{eventid}/')
+    .onWrite((change, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const beforeData = change.before.val(); 
+      const afterData = change.after.val();
+
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      //return change.ref.parent.child('uppercase').set(event);
+      if(afterData !== null ) {
+        //findUserIdsForNames(afterData, context.params.ecclesiaId)
+        checkIfNotificationNeeded(beforeData, afterData, context.params.ecclesiaId, context.params.eventid)
+      }
+
+      return true
+});
+
+function checkIfNotificationNeeded(beforeData, afterData, ecclesiaId, eventId){
+    if (beforeData.venue !== afterData.venue){
+        console.log("will notify")
+        sendNotification(ecclesiaId, eventId, afterData.venue)
+    }
+}
+
+function sendNotification(topic, eventId, newVenue){
+
+    // See documentation on defining a message payload.
+
+    var message = {
+        notification: {
+          title: 'Change to the plan',
+          body: 'CYC this weekend is now at ' + newVenue,
+        },
+        android: {
+          ttl: 3600 * 1000,
+          notification: {
+            icon: 'stock_ticker_update',
+            color: '#f45342',
+          },
+        },
+        apns: {
+          payload: {
+            aps: {
+              badge: 0,
+            },
+          },
+        },
+        topic: topic
+      };
+
+    // Send a message to devices subscribed to the provided topic.
+    admin.messaging().send(message)
+    .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+        console.log('Error sending message:', error);
+    });
+}
+
+// function findUserIdsForNames(event, ecclesiaId){
+//     for(let i = 0; i < userFields[event.type].length; i++){
+//         let userType = userFields[event.type][i]
+
+//         if (event[userType].name !== null){
+//             console.log(event[userType].name)
+//             findIdForName(event[userType].name, ecclesiaId)
+//         } else {
+//             console.log("not single" + event[userType])
+//             let userArray = event[userType]
+//             for(let j = 0; j < userArray.length; j++){
+//                 console.log(userArray[j].name)
+//             }
+//         }
+//     }
+// }
+
+// function findIdForName(name, ecclesiaId){
+//     admin.database().ref('/members/' + ecclesiaId).once('value').then(function(snapshot) {
+//         let members = snapshot.val()
+//         for(let i = 0; i < members.length; i++){
+//             if((_.invert(hash))[name] !== null){
+//                 console.log((_.invert(hash))[name])
+//             }
+//         }
+//     });
+// }
